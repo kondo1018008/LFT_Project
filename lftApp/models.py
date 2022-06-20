e933028fd7ae52132274b962ee170141961542a0
ShogoKondo <koiru4423syougokondou@yahoo.co.jp>
2022-06-20 23:13:15 +0900

    [fix]models

--- lftApp/models.py
+++ lftApp/models.py
@@ -3,10 +3,24 @@ from django.utils import timezone
 import datetime
 
 # Create your models here.
+class Genre(models.Model):
+    genre_title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return  self.genre_title
+
+
+
+class Game(models.Model):
+    game_title = models.CharField(max_length=200)
+    game_genre = models.
+    def __str__(self):
+        return self.game_title
 
 
 class Player(models.Model):
     player_name = models.CharField(max_length=100)
+    player_twitter_link = models.URLField()
 
     def __str__(self):
         return self.player_name
@@ -14,6 +28,7 @@ class Player(models.Model):
 class LFT(models.Model):
     lft_name = models.CharField(max_length=100)
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
+
 
     def __str__(self):
         return
