d6c599c762b9f85d5dc4c4108d2bc42b09a6dd63
ShogoKondo <koiru4423syougokondou@yahoo.co.jp>
2022-06-22 02:17:32 +0900

    [add]各モデルにMetaクラスを追加

--- lftApp/models.py
+++ lftApp/models.py
@@ -3,16 +3,15 @@ from django.utils import timezone
 import datetime
 
 # Create your models here.
-class Genre(models.Model):
-    genre_title = models.CharField(max_length=100)
-
-    def __str__(self):
-        return  self.genre_title
-
-
 
 class Game(models.Model):
     game_title = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'game'
+        verbose_name = 'ゲームタイトル'
+        verbose_name_plural = 'ゲームタイトル群'
+
     def __str__(self):
         return self.game_title
 
@@ -21,16 +20,38 @@ class Player(models.Model):
     player_name = models.CharField(max_length=100)
     player_twitter_link = models.URLField()
 
+    class Meta:
+        db_table = 'player'
+        verbose_name = 'プレイヤー'
+        verbose_name_plural = 'プレイヤー群'
+
+
     def __str__(self):
         return self.player_name
+
+
+class Player_Game(models.Model):
+    Player = models.ForeignKey(Player, on_delete=models.CASCADE())
+    Game = models.ForeignKey(Game, on_delete=models.CASCADE())
+
+    class Meta:
+        db_table = 'player_game_relation'
+
 
 class LFT(models.Model):
     lft_name = models.CharField(max_length=100)
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE())
+    description = models.TextField()
+
+    class Meta:
+        db_table = 'lft'
+        verbose_name = 'LFT'
+        verbose_name_plural = 'LFT群'
 
 
     def __str__(self):
-        return
+        return self.lft_name
 
     def was_published_recently(self):
         now = timezone.now()
